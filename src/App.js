c696a23fa57c739c1ef15a0607374f7fbf83f4ca
Ali Qumail <aliqumail@gmail.com>
2021-04-15 01:26:35 +0500

    fixed center alignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import GameBoard from "./Components/GameBoard";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Row} from 'react-bootstrap';
 
 import "./styles.css";
 
 export default function App() {
   document.body.style = "background-color:blue";
   return (
-    <div className="main">
-      <h3 className="heading">Tic Tac Toe</h3>
-      <div className="mainBoard">
+    <Container className="">
+      <Row className="justify-content-center"><span className='heading'>Tic Tac Toe</span></Row>
+      <Row className="justify-content-center">
         <GameBoard />
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 }
